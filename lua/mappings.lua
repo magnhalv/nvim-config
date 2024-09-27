3056cca00ab088ab0cef3f697926d4eeb95c39bc
Magnus Halvorsen <magnus.halvorsen@sintef.no>
2024-09-27 10:55:41 +0200

    Add close all buffers

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,22 @@
 require "nvchad.mappings"
 
+function CloseOtherBuffers()
+  local current_buf = vim.fn.bufnr "%"
+  for _, buf in ipairs(vim.fn.getbufinfo { bufloaded = true }) do
+    if buf.bufnr ~= current_buf then
+      vim.api.nvim_buf_delete(buf.bufnr, {})
+    end
+  end
+end
+
 -- add yours here
 
 local map = vim.keymap.set
-
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
+map(
+  "n",
+  "<Leader>bd",
+  ":lua CloseOtherBuffers()<CR>",
+  { desc = "Close all other buffers", noremap = true, silent = true }
+)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
