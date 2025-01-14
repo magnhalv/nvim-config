29644cf2afa291e36271f3ba02e676208ccbe146
Magnus Halvorsen <magnus.halvorsen@sintef.no>
2025-01-14 11:11:42 +0100

    Add multiple cursor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -136,4 +136,21 @@ return {
       },
     },
   },
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
 }
